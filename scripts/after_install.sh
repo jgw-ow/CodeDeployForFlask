9e511fc0c16acc5fa2991a613fad6d3746e969a1
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2023-11-23 15:19:09 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y cron
 
 cd   /home/ubuntu/hello-flask
 
 echo ">>> pip install ----------------------"
 pip install -r requirements.txt
+
+(crontab -l ; echo "* * * * * /usr/bin/python3 /hello-flask/historyUpdate.py >> /var/log/cron.log 2>&1") | crontab -
 
 
 echo ">>> remove template files ------------"
