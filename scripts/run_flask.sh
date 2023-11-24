20ffa9631c0701ba5cc95924d5b32253c1f572de
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2023-11-24 14:14:39 +0900

    Update run_flask.sh

--- scripts/run_flask.sh
+++ scripts/run_flask.sh
@@ -6,4 +6,5 @@ cd /home/ubuntu/hello-flask
 
 echo ">>> run app --------------------------"
 cron
-python3 -u app.py > /dev/null 2> /home/ubuntu/flask.log < /dev/null &
+# python3 -u app.py > /dev/null 2> /home/ubuntu/flask.log < /dev/null &
+python3 -u app.py > /home/ubuntu/flask.log 2> /home/ubuntu/flask.log < /dev/null &
